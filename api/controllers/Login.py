ea1d719c5a2d72879ddd1abf1ac710a4a0afd13c
Kavya6805 <kavymodi6805@gmail.com>
2025-02-21 23:08:23 +0530

    modified login response

--- api/controllers/Login.py
+++ api/controllers/Login.py
@@ -1,15 +1,16 @@
-from flask import jsonify,request
+from flask import jsonify, request
 from api.models.loginModel import Login
 from api.response import apiResponse
 
 
 def login():
-    username=request.form['username']
-    password=request.form['password']
-    login=Login()
-    data=login.login(username,password)
-    if data != None:
-        return apiResponse(True,"User Authenticated Successfully",data)
+    username = request.form['username']
+    password = request.form['password']
+    login = Login()
+    data = login.login(username, password)
+    if data == "Wrong Password!!":
+        return apiResponse(False, data, "")
+    elif data == "User Not Exist!!":
+        return apiResponse(False, data, "")
     else:
-        return apiResponse(False,"Username OR Password Wrong !!",None)
-    
+        return apiResponse(True, "User Authenticated Successfully", data)
--- api/models/loginModel.py
+++ api/models/loginModel.py
@@ -2,40 +2,48 @@ from sqlalchemy import text
 from database import db
 from flask import session
 import jwt
-from datetime import timedelta,datetime
+from datetime import timedelta, datetime
 from app import app
 from functools import wraps
 
-class Login:
-    def login(self,username,password):
-        if self.authenticate(username,password):
-            session["logged_in"]=True
-            token=jwt.encode(payload={
-                'username':username,
-                'expiration':str(datetime.utcnow()+timedelta(seconds=120))
-            },
-            key=app.config['SECRET_KEY'],algorithm='HS256')
-            print(token)
-            session['token']=token
-            return token
-        else:
-            return None
 
-    def authenticate(self,username,password):
-        sql=text("SELECT * FROM USERSLOGINREGISTER WHERE username=:username and password=:password")
-        result=db.session.execute(sql.params(username=username,password=password))
-        if result.fetchone():
-            return True
+class Login:
+    def login(self, username, password):
+        if self.authenticate(username, password) == 0:
+            session["logged_in"] = True
+            token = jwt.encode(payload={
+                'username': username,
+                'expiration': str(datetime.utcnow()+timedelta(seconds=120))
+            },
+                key=app.config['SECRET_KEY'], algorithm='HS256')
+            print(token)
+            session['token'] = token
+            return token
+        elif self.authenticate(username, password) == 1:
+            return "Wrong Password!!"
         else:
-            return False
-        
-    def auth(self,func):
+            return "User Not Exist!!"
+
+    def authenticate(self, username, password):
+        sql1 = text("SELECT * FROM USERLOGINREGISTER WHERE username=:username")
+        result1 = db.session.execute(sql1.params(username=username))
+        if result1.fetchone():
+            sql2 = text(
+                "SELECT password FROM USERLOGINREGISTER WHERE username=:username and password=:password")
+            result2 = db.session.execute(
+                sql2, {'username': username, 'password': password})
+            if result2.fetchone():
+                return 0
+            else:
+                return 1
+        else:
+            return 2
+
+    def auth(self, func):
         @wraps(func)
-        def decorated(*args,**kwargs):
-            token=session['token']
+        def decorated(*args, **kwargs):
+            token = session['token']
             if not token:
                 pass
             else:
                 pass
-
-
