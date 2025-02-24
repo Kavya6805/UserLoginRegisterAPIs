6ff89f6edb25eea183935c6a35a01610951f9198
Kavya Modi <kavymodi6805@gmail.com>
2025-02-24 11:47:16 +0530

    new

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from api.controllers import Login
+from api.controllers import Register
 
 bp=Blueprint('api',__name__,url_prefix='/api')
 
 bp.add_url_rule('/login',view_func=Login.login,methods=["POST"])
+bp.add_url_rule('/registeruser',view_func=Register.register,methods=["POST"])
 
